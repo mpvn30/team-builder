f8d9f775dd44dc15696507d124b88bd0b3678a8d
Mike Nguyen <mpvnguyen1996@gmail.com>
2019-09-18 17:10:57 -0400

    added form from form.js and new member form from members.js

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./App.css"
+import Members from "./components/members";
+import Form from "./components/form";
+
 
 function App() {
+  const [member, setMember] = useState([
+    {
+      id: 1,
+      name: "Mike",
+      email: "mpvnguyen1996@gmail.com",
+      role: "front-end developer",
+      motto: "It's not the pineapple, it's the pizza"
+    }
+  ]);
+
+  const addNewMember = add => {
+    const newMember = {
+      id: add.id,
+      name: add.name,
+      email: add.email,
+      role: add.role,
+      motto: add.motto
+    };
+    setMember([...member, newMember]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="form-container">
+        <h1>Membership Form</h1>
+        <Form addNewMember={addNewMember} />
+        <Members member={member} />
+      </div>
     </div>
   );
 }
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
 export default App;
---
+++ team-builder/src/components/members.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Members = props => {
+  return (
+    <div>
+      {props.member.map(e => (
+        <div key={e.id} className='member'>
+          <h2>{e.name}</h2>
+          <p>E-mail: {e.email}</p>
+          <p>Role: {e.role}</p>
+          <p>Motto: "{e.motto}"</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Members;
